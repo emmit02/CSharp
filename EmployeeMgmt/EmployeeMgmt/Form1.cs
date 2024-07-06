f4cefdda7cd354361fc3698487588cb7c983e402
Sanjeevani <sanjeevanisharma741390pal@gmail.com>
2024-07-07 01:07:34 +0530

    empmgmt

--- EmployeeMgmt/EmployeeMgmt/Form1.cs
+++ EmployeeMgmt/EmployeeMgmt/Form1.cs
@@ -8,13 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace HostelMgmtLearn
+namespace EmployeeMgmt
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
